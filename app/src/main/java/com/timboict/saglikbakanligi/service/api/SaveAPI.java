4007fc687d8be09e2556a182a6922cb01f983299
Mustafa Guven <mustafaguven@gmail.com>
2015-05-24 23:45:46 +0300

    retrofit

--- app/src/main/java/com/timboict/saglikbakanligi/service/api/SaveAPI.java
+++ app/src/main/java/com/timboict/saglikbakanligi/service/api/SaveAPI.java
@@ -3,6 +3,7 @@ package com.timboict.saglikbakanligi.service.api;
 import com.timboict.saglikbakanligi.model.ISBSAritmaModel;
 
 import retrofit.Callback;
+import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.POST;
 import retrofit.http.Path;
@@ -14,5 +15,5 @@ public interface SaveAPI<T> {
 
     @POST(EndPoint.SAVE)
     void saveAritma(@Path("name") String name,
-                    ISBSAritmaModel model, Callback<ISBSAritmaModel> response);
+                    @Body ISBSAritmaModel model, Callback<ISBSAritmaModel> response);
 }
